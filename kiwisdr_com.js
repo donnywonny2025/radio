7cecd087632473cfc872a5ed828f92f85cdd19c7
jeff kerr <jeffkerr@jeffs-Mac-mini.local>
2026-03-12 19:44:40 -0400

    Initial commit of Leaflet dyatlov map

---
+++ kiwisdr_com.js
@@ -0,0 +1,8 @@
+// KiwiSDR.com receiver list for dyatlov map maker
+// Automatically generated from http://kiwisdr.com/public/
+// File generation timestamp: Thu Mar 12 23:43:11 2026
+
+var kiwisdr_com =
+[
+]
+;
